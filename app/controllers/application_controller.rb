d435286ca3ff53eefeba82061950e976bc850fd2
Michal Zaporski <focus16k@gmail.com>
2021-05-10 23:55:05 +0200

    login and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # because of making an API
+  skip_before_action :verify_authenticity_token
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_epic_api", domain: ""
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_epic_api", domain: ""
+else
+  Rails.application.config.session_store :cookie_store, key: "_epic_api"
+end
